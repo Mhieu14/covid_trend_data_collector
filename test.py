1f9c3da700311f9e5a3109567bb4d8f5c99c57ec
buiminhhieuno5 <buihieu@quangich.com>
2022-01-02 22:21:15 +0700

    delete data files

--- test.py
+++
@@ -1,8 +0,0 @@
-from pytrends.request import TrendReq
-
-pytrends = TrendReq(hl='en-US', tz=360)
-
-kw_list = ["Blockchain"]
-result = pytrends.build_payload(['Coronavirus'], timeframe='2020-02-01 2020-03-01', geo='US')
-df = pytrends.interest_over_time()
-print(df)
